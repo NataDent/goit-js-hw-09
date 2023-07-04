d9b7263f772ea7e3e188eb142b7e48c911d96c1f
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-04 22:13:44 +0300

    task 1 is running

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+console.log(btnStart)
+const btnStop = document.querySelector('button[data-stop]');
+console.log(btnStop);
+const body = document.body;
+console.log(body);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', onclick);
+
+function onclick(e) {
+     e.target.dataset.disabled = true;
+    const timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+   
+    }, 1000); 
+    console.dir(btnStart);
+    
+}
