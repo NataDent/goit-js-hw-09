86718c45e8210003ea05a997c8bf36bb9f6a6b17
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-08 00:43:04 +0300

    Task 2 doesn't work yet'

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -21,17 +21,47 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+selectors.input.value = '';
+  
+  const fp = flatpickr(selectors.input, options);
 
-console.log(options);
-
-const fp = flatpickr(selectors.input, options);
-
-selectors.btn.addEventListener('click', onClick);
+  selectors.btn.addEventListener('click', onClick);
 
 function onClick() {
-    setInterval(() => {
-    const currentDate = new Date();
-const targetDate = new Date(selectors.input.value);
-   console.log(Date.now(targetDate)) 
-   }, 1000) 
+  const targetDate = new Date(selectors.input.value);
+  const currentDate = new Date();
+  const leftTime = targetDate.getTime() - currentDate.getTime();
+ 
+if (targetDate <= currentDate) {
+  window.alert('Please choose a date in the future')
+  return;
+  } 
+  const timer = setInterval(() => {
+    
+  }, 1000)
+} 
+
+
+
+function convertMs(ms) {
+  
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}    
