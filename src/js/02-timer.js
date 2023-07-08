bb9bce83632a540ee40ab1b614a72ec5ac54fbc3
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-08 18:08:14 +0300

    task2 fixed after tutor's audit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,34 +11,48 @@ const selectors = {
     minutes: document.querySelector('span[data-minutes]'),
     seconds: document.querySelector('span[data-seconds]')
 }
-
+let startTime = 0;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+
+    onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startTime = selectedDates[0].getTime();
+      selectors.btn.removeAttribute('disabled');
+    }
   },
 }; 
+
   const fp = flatpickr(selectors.input, options);
 
   selectors.btn.addEventListener('click', onClick);
 
-function onClick(e) {
+function onClick() {
   selectors.input.value = '';
-  if (fp.selectedDates[0] <= Date.now()) {
-    e.target.disabled = true;
-    window.alert('Please choose a date in the future')
-  }
-  e.target.disabled = false;
-    const timer = setInterval(() => {
+
+  
+  const timer = setInterval(() => {
+       
     const leftTime = fp.selectedDates[0].getTime() - Date.now();
-      convertMs(leftTime);
-      selectors.days.textContent = days;
-      selectors.hours.textContent = hours;
-      selectors.minutes.textContent = minutes;
-      selectors.seconds.textContent = seconds;
+      
+    const time = convertMs(leftTime);
+    
+    const days = time.days;
+    const hours = time.hours;
+    const minutes = time.minutes;
+    const seconds = time.seconds;
+
+    selectors.days.textContent = days.toString().padStart(3,0);
+    selectors.hours.textContent = hours.toString().padStart(2,0);
+    selectors.minutes.textContent = minutes.toString().padStart(2,0);
+    selectors.seconds.textContent = seconds.toString().padStart(2,0);
+
+    
     }, 1000)
   
 } 
@@ -62,8 +76,7 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+ 
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}    
+
